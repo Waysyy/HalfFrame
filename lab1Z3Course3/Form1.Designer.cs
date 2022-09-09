d2cd296d02b1bc74147384435278d8a5509217c9
Waysyy <annatkach225@gmail.com>
2022-09-09 12:09:18 +0700

    .~.

---
+++ lab1Z3Course3/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace lab1Z3Course3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Рамка 1",
+            "Рамка 2",
+            "Рамка 3"});
+            this.listBox1.Location = new System.Drawing.Point(93, 118);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(56, 43);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 16);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(333, 96);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ lab1Z3Course3/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1Z3Course3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string[] symbols = { "Рамка 1", "Рамка 2", "Рамка 3" };
+            listBox1.Items.AddRange(symbols);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string vivod = "";
+            string vivod2 = "";
+            string[] rtfStrings = richTextBox1.Text.Split(new char[] { '\n' });
+            string[] storona = new string[rtfStrings.Length];
+            string verh = "";
+
+            for(int i = 0; i < rtfStrings.Length; ++i)
+            {
+                string tmp = rtfStrings[0];
+                string MaxLenght = rtfStrings[i];
+                if (MaxLenght.Length > tmp.Length) 
+                {
+                    tmp = MaxLenght;
+                }
+                verh = tmp;
+            }
+
+            for (int i = 0; i< rtfStrings.Length; ++i )
+            {
+                storona[i] = "|";
+            }
+            for (int i = 0; i < storona.Length; ++i)
+            {
+                string MaxL = "";
+                MaxL = rtfStrings[i];
+
+                vivod += Convert.ToString(storona[i]) + " " + rtfStrings[i];
+                    for (int j = 0; j < verh.Length; ++j)
+                    {
+                     
+                    if (MaxL.Length < verh.Length+1 && MaxL.Length != verh.Length)
+                        {
+                            vivod += "  ";
+                            MaxL += "1";
+                        }
+                    }
+                vivod += Convert.ToString(storona[i]) + "\n";
+                
+                /*vivod += Convert.ToString(storona[i]) + empty.PadRight(verh, ' ') + rtfStrings[i]
+                + empty.PadRight(verh*2, ' ') + Convert.ToString(storona[i]) + "\n";*/
+                
+                
+                
+            }
+            for (int i = 0; i < verh.Length; ++i)
+            {
+                vivod2 += " -";
+            }
+            
+            string selectedSymbols = listBox1.SelectedItem.ToString();
+            MessageBox.Show( vivod2 + "\n" + vivod +  vivod2);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
